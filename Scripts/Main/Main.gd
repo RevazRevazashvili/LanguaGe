b1537f9b8cb90313fc0e52653f5750a67a1f1b5c
Cx3n1 <lukagobronidze@gmail.com>
2023-04-09 23:41:49 +0400

    added loading utility

--- Scripts/Main/Main.gd
+++ Scripts/Main/Main.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Test/TestScene.tscn")
+	print("button pressed")
+	$Timer.start(5)
+	LoadingScreen.start_loading("res://Scenes/Test/TestScene.tscn")
+	await $Timer.timeout
+	LoadingScreen.stop_loading()
