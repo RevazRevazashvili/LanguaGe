c5ee5793dd9a256c8509e6aa47e4fb98622d3950
sfruff <armanme@mail.ru>
2023-06-13 20:22:48 +0400

    fixing some bugs, and adding the logo

--- Scripts/TopBar/TopBar.gd
+++ Scripts/TopBar/TopBar.gd
@@ -3,8 +3,8 @@ extends Control
 
 
 func _ready() -> void:
-	$Label.text = Watchman.get_current_header()
-	$TextureButton.visible = Watchman.is_prevous_enabled()
+	$Control.get_node("Label").text = Watchman.get_current_header()
+	$Control.get_node("TextureButton").visible = Watchman.is_prevous_enabled()
 	
 
 func _on_texture_button_pressed() -> void:
